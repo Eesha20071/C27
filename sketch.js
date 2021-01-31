c8970aa1be9681dc2af91ae1c2dc40b15369d6b8
Eesha20071 <naik.vishal.s@gmail.com>
2021-01-31 18:51:15 +0530

    created Constrain

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint; 
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6;
+var chain;
+var chain2; 
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -24,6 +28,7 @@ function setup(){
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
+
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -35,6 +40,12 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+    log6 = new Log(230,180,80, PI/2);
+
+    chain = new Chain(bird.body, log6.body);
+
+    chain2 = new Chain (bird.body, pig1.body); 
 
 }
 
@@ -61,4 +72,11 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
+
+    chain.display();
+    chain2.display();
+
+    
 }
